d27202519eca963d86b7fa99d34ef7ee1ed7ab6f
Sean Haughey <sdhaughey@gmail.com>
2016-01-23 09:35:57 -0600

    first commit

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,33 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by SEAN HAUGHEY on 1/23/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+
+// #1
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    // #2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        // #3
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    // #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
